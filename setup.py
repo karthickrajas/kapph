a4cac1c990c17812af0f1f89953844e12389b84f
karthickrajas <karthick11b36@gmail.com>
2018-08-04 20:47:53 +0530

    adding version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Mon Jul  2 12:59:11 2018
 from setuptools import setup
 
 setup(name='KAPPH',
-      version='0.1',
+      version='0.101',
       description='Tool for Pre-Processing',
       url='http://github.com/karthickrajas/kapph',
       author='Karthick Raja',
